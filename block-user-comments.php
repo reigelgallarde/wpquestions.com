365ffa6ce7ef094bb673df28d77d46154063ad4d
Reigel Gallarde <reigelgallarde@gmail.com>
2015-02-15 16:35:06 +0800

    Update block-user-comments.php

--- block-user-comments.php
+++ block-user-comments.php
@@ -1,11 +1,17 @@
 <?php
 
+
 function block_user_in_comments_form($return){
 	global $comment, $post, $current_user;
-    if ( current_user_can('moderate_comments') || ($current_user->ID == $post->post_author)) { // you might need changing this line
+	
+	if ($post->post_author == $comment->user_id) {return  $return;} // don't do for post author
+	$comment_user = new WP_User( $comment->user_id );
+	if (in_array('administrator', $comment_user->roles)) {return  $return;} // don't do for admin...
+	$block_users = get_post_meta($post->ID, 'block-user-comments', true );
+	
+    
+	if ( current_user_can('moderate_comments') || ($current_user->ID == $post->post_author)) { // you might need changing this line
 
-		if ($post->post_author === $comment->user_id) {return;} // don't do for post author
-		$block_users = get_post_meta($post->ID, 'block-user-comments', true );
 		$checked = ' ';
 		
 		if (is_array($block_users) && !empty($block_users) && in_array($comment->user_id, $block_users)) {
@@ -13,22 +19,40 @@ function block_user_in_comments_form($return){
 		}
 		
 		$form = '<form method="POST" action="'.get_the_permalink($post->ID).'">';
-		$form .= 'Block user? <input type="checkbox" name="block-user-comments" '.$checked.' value="yes" />';
+		$form .= 'Block user? <input type="checkbox" name="block-user-comments" class="blk_user_comments" '.$checked.' value="yes" />';
 		$form .= '<input type="hidden" name="user_id" value="'.$comment->user_id.'" />';
 		$form .= '<input type="hidden" name="the_post" value="'.$post->ID.'" />';
 		$form .= wp_nonce_field( 'block-user-comments', '_wpnonce', true, false );
-		$form .= ' <input type="submit" value="submit" name="submit" /></form>';
+		$form .= ' <input type="submit" value="submit" name="submit" class="submit_blk_user_comments" /></form>';
+		?>
+			<script>
+				jQuery(function($){
+					$('.submit_blk_user_comments').hide();
+					$('.blk_user_comments').change(function(){
+						$(this).siblings('.submit_blk_user_comments').show();
+					});
+				});
+			</script>
+		<?php
 		$return .= $form;
+	} elseif (is_array($block_users) && !empty($block_users) && in_array($comment->user_id, $block_users)) {
+		$return . '<p>"You have been blocked - You can not leave a comment on this post" </p>';
 	}
     return $return;
 }
-add_filter('get_comment_author_link', 'block_user_in_comments_form');
+add_filter('get_comment_author', 'block_user_in_comments_form');
 
 function block_user_in_comments(){
 	if ( isset( $_POST['_wpnonce'] ) && wp_verify_nonce( $_POST['_wpnonce'], 'block-user-comments' ) ) {
 		if (isset($_POST['user_id']) && isset($_POST['the_post'])) {
 			$user_id = sanitize_text_field($_POST['user_id']);
 			$post_id = sanitize_text_field($_POST['the_post']);
+			
+			$comment_user = new WP_User( $user_id );
+			if (in_array('administrator', $comment_user->roles)) {return;} // don't do for admin...
+			$post = get_post($post_id);
+			if ($post->post_author == $user_id) {return;} // don't do for post author
+			
 			$block_users = get_post_meta($post_id, 'block-user-comments', true );
 			if ($_POST['block-user-comments'] === 'yes') {
 				if (is_array($block_users) && !empty($block_users)) {
@@ -38,8 +62,8 @@ function block_user_in_comments(){
 				}
 			} elseif (is_array($block_users) && !empty($block_users)) {
 				$key = array_search($user_id, $block_users);
-				if (isset($key))
-				unset($block_users[$key]);
+				if ($key != NULL || $key !== FALSE)
+					unset($block_users[$key]);
 			}
 			update_post_meta($post_id, 'block-user-comments', $block_users );
 		}
@@ -49,15 +73,16 @@ add_filter('template_redirect', 'block_user_in_comments');
 
 function my_comments_open( $open, $post_id ) {
 	global $current_user;
+	
 	$block_users = get_post_meta($post_id, 'block-user-comments', true );
 	if (is_array($block_users) && !empty($block_users)) {
 		$key = array_search($current_user->ID, $block_users);
-		if (isset($key)) {
+		if ($key != NULL || $key !== FALSE) {
 			$open = false;
 		}
 	}
 
 	return $open;
 }
-add_filter( 'comments_open', 'my_comments_open', 10, 2 );
+add_filter( 'comments_open', 'my_comments_open', 1, 2 );
 ?>
